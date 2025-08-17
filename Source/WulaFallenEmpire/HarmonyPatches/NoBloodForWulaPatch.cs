f1568fdfebdf3c786c61764c7901fd874551d6a0
ProjectKoi-Kalo\Kalo <kalospacer@gmail.com>
2025-08-17 12:51:38 +0800

    暂存补丁反过来

--- Source/WulaFallenEmpire/HarmonyPatches/NoBloodForWulaPatch.cs
+++ Source/WulaFallenEmpire/HarmonyPatches/NoBloodForWulaPatch.cs
@@ -4,19 +4,15 @@ using System.Reflection;
 
 namespace WulaFallenEmpire
 {
-    [HarmonyPatch(typeof(Pawn_HealthTracker), "get_CanBleed")]
-    public static class NoBloodForWula_CanBleed_Patch
+    [HarmonyPatch(typeof(Hediff_Injury), "get_BleedRate")]
+    public static class NoBloodForWula_BleedRate_Patch
     {
-        public static void Postfix(Pawn_HealthTracker __instance, ref bool __result)
+        public static void Postfix(Hediff_Injury __instance, ref float __result)
         {
-            // 使用反射获取Pawn_HealthTracker的私有pawn字段
-            Pawn pawn = (Pawn)typeof(Pawn_HealthTracker).GetField("pawn", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(__instance);
-
-            if (pawn.def.defName == "WulaSpecies")
+            if (__instance.pawn.def.defName == "WulaSpecies")
             {
-                __result = false;
+                __result = 0f;
             }
         }
     }
-
 }
