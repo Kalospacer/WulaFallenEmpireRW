a670ceb38eacfaaeaa2eced2f13c3aa8eb995e19
ProjectKoi-Kalo\Kalo <kalospacer@gmail.com>
2025-12-14 14:56:53 +0800

    chore: normalize Dialog_CustomDisplay encoding

--- Source/WulaFallenEmpire/EventSystem/Dialog_CustomDisplay.cs
+++ Source/WulaFallenEmpire/EventSystem/Dialog_CustomDisplay.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -666,3 +666,4 @@ namespace WulaFallenEmpire
         }
     }
 }
+
